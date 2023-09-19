9b07feebd97cc7add8cced93c18d20e46e3eece9
juan hernandez <juanhernandezbravo750@gmail.com>
2023-09-18 18:19:44 -0600

    inicializa el proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en el puerto ${port}`);
+});
